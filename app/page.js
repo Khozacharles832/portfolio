c8bed0df4a33ca02f57f38e38d8894a0ecacfd04
Charlie <khozacharles832@gmail.com>
2026-08-17 11:34:30 +0200

    Create header section

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 "use client";
+import Header from "./comoponents/Header";
 import Navbar from "./comoponents/Navbar";
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <Header />
     </>
   );
 }
